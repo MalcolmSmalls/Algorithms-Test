34144893c3150a2a94ba4c50471ba26a3c434da3
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-13 12:00:40 -0800

    Finish calculate factorial

--- js/main.js
+++ js/main.js
@@ -33,12 +33,39 @@
 
   //4
 
-function sumTo(x) {
-    if(x === 1){
+// function sumToOne(x){
+//     let total = 0
+//     for(let i = 1; i <= x; i++){
+//         total = total + i
+//     }
+//     return total
+// }
+
+
+// 0 + 1 = 1 (new Total)
+// 1 + 2 = 3 (new Total)
+// 3 + 3 = 6 (new Total)
+// 6 + 4 = 10 (new Total)
+// returns 10
+
+
+// function sumTo(x) {
+//     if(x === 1){
+//         return x
+//     }else{
+//         return x + sumTo(x-1)
+//     }
+// }
+
+// console.log(sumToOne(4))
+
+function newFactorial(x){
+    let n = x
+    if(n == 1 ){
         return x
     }else{
-        return x + sumTo(x-1)
+        return x * newFactorial(n-1)
     }
 }
 
-console.log(sumTo(4))
+console.log(newFactorial(5))
