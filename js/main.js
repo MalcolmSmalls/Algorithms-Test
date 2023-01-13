fec9cb169564cc062d6488ae435f040176d3a27c
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-13 15:35:27 -0800

    Finish collatz recursive function

--- js/main.js
+++ js/main.js
@@ -59,13 +59,71 @@
 
 // console.log(sumToOne(4))
 
-function newFactorial(x){
-    let n = x
-    if(n == 1 ){
-        return x
-    }else{
-        return x * newFactorial(n-1)
-    }
+// function newFactorial(x){
+//     let n = x
+//     if(n == 1 ){
+//         return x
+//     }else{
+//         return x * newFactorial(n-1)
+//     }
+// }
+
+// console.log(newFactorial(5))
+
+// function countDown(x){
+//     if(x === 1){
+//         return x // this returns x which would be 1 at this case,
+//     } else {
+//         console.log(x)
+//         return countDown(x-1) // i guess this goes all the way down to 1-1, which means x is 1 for some reason then it hits the base
+//     }
+// }
+
+// console.log(countDown(10))
+
+
+// function fib(n){
+//     if(n == 1){
+//         return n
+//     }else{
+//         return n + fib(n-1)
+//     }
+// }
+
+// console.log(fib(5))
+
+/* first time around 
+x === 3
+so 3 + (2)
+then
+2 + 1
+then return 1
+
+*/
+
+// function sumRange(x){
+//     if(x===1){
+//         return x
+//     }else{
+//         return x + sumRange(x-1)
+//     }
+// }
+
+// console.log(sumRange(3))
+
+
+function collatz(n, total = 0){
+	if(n==1){
+        console.log(n)
+		return total
+	}else if(n % 2 === 0){
+        console.log(n)
+		return collatz(n/2, total + 1)
+	}else{
+        console.log(n)
+		return collatz(3*n+1, total + 1)
+	}
+
 }
 
-console.log(newFactorial(5))
+console.log(collatz(7))
